54738d482019b70eeda284ee1bb9132535d5f55e
TRMSC <86586942+TRMSC@users.noreply.github.com>
2022-03-10 22:30:02 +0100

    Create cloud-backup-settings.py

---
+++ cloud-backup-settings.py
@@ -0,0 +1,24 @@
+# Cloud Backup v.1.1.filedev - Copyright (C) 2022, TRMSC - https://trmsc1.wordpress.com/ 
+# GNU General Public Licence 3.0 - http://www.gnu.de/documents/gpl-3.0.en.html 
+
+import configparser
+
+print ("Welcome to the settings for your cloud-backup\n")
+print ("v.1.1")
+
+print ("\nGENERAL")
+print ("1 - Change directory")
+print ("2 - Number of stored backups")
+print ("\nACCESS")
+print ("3 - Set username")
+print ("4 - Set password")
+print ("\nADRESSBOOK")
+print ("6 - Activate adressbook backup")
+print ("5 - Adressbook url")
+print ("\nCALENDAR")
+print ("6 - Activate calendar backup")
+print ("7 - Calendar url")
+print ("8 - Calendar list")
+print ("\nDATA")
+print ("9 - Activate data backup from desktop client")
+print ("10 - Client path")
